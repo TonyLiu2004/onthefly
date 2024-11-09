5f9851a82287d17b6432981c5971aea4e3914e35
TonyLiu2004 <tony.liu404@gmail.com>
2024-11-09 10:56:00 -0500

    trying to print to see where the problem is

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      {console.log("QWEWQEWQEWQEQW 1")}
       <App />
     </BrowserRouter>
   </React.StrictMode>
